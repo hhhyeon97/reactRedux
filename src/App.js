f2fd584d5d46a1313c739613fe514eeecb0e0767
jihyeon <kth083030@naver.com>
2024-03-31 10:50:26 +0900

    리덕스하이

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import './App.css';
-import { useState } from 'react';
+//import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Box from './component/Box';
 
 function App() {
   // const [count, setCount] = useState(0);
   const count = useSelector((state) => state.count);
-  let id = useSelector((state) => state.id);
-  let pw = useSelector((state) => state.pw);
+  // let id = useSelector((state) => state.id);
+  // let pw = useSelector((state) => state.pw);
   const dispatch = useDispatch();
   // type 속성은 필수 / payload는 함수의 매개변수 같은 느낌, 선택적인 요소임
   const increase = () => {
-    dispatch({ type: 'INCREMENT', payload: { num: 5 } });
+    dispatch({ type: 'INCREMENT' });
     // setCount(count + 1);
   };
-
-  const login = () => {
-    dispatch({ type: 'LOGIN', payload: { id: 'gamza', pw: '1234' } });
+  const decrease = () => {
+    dispatch({ type: 'DECREMENT' });
   };
-
+  // const login = () => {
+  //   dispatch({ type: 'LOGIN', payload: { id: 'gamza', pw: '1234' } });
+  // };
+  const increase10 = () => {
+    dispatch({ type: 'INCREMENT10' });
+  };
+  const decrease10 = () => {
+    dispatch({ type: 'DECREMENT10' });
+  };
   return (
     <div className="container">
       <div className="wrap">
-        <h2>{count}</h2>
-        <span>
-          id : {id} pw : {pw}
-        </span>
-        <button onClick={increase}>증가!</button>
-        <button onClick={login}>LOGIN</button>
-        <Box />
+        <h1>{count}</h1>
+
+        <button onClick={increase}>▲</button>
+        <button onClick={decrease}>▼</button>
+        <button onClick={increase10}>10 up!</button>
+        <button onClick={decrease10}>10 down!</button>
+        {/* <button onClick={login}>LOGIN</button> */}
+        {/* <Box /> */}
       </div>
     </div>
   );
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -5,13 +5,26 @@ let initialState = {
 };
 
 function reducer(state = initialState, action) {
-  console.log('action은 뭐야?', action);
-  if (action.type === 'INCREMENT') {
-    return { ...state, count: state.count + action.payload.num };
-  } else if (action.type === 'LOGIN') {
-    return { ...state, id: action.payload.id, pw: action.payload.pw };
+  //console.log('action은 뭐야?', action);
+  // if (action.type === 'INCREMENT') {
+  //   return { ...state, count: state.count + 1 };
+  // } else if (action.type === 'LOGIN') {
+  //   return { ...state, id: action.payload.id, pw: action.payload.pw };
+  // } else if (action.type === 'DECREMENT') {
+  //   return { ...state, count: state.count - 1 };
+  // }
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 };
+    case 'INCREMENT10':
+      return { ...state, count: state.count + 10 };
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 };
+    case 'DECREMENT10':
+      return { ...state, count: state.count - 10 };
+    default:
+      return { ...state };
   }
-  return { ...state };
 }
 
 export default reducer;
